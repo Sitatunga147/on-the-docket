44b188347573114d6f23e79773e29840312be863
Sitatunga147 <elliot.wymore@gmail.com>
2025-06-27 09:07:38 -0500

    Add JTabbedPane with two tabs
    Added a JTabbedPane with tabs for "Calendar" and "Analytics" to the MainView.

--- src/onthedocket/visuals/MainView.java
+++ src/onthedocket/visuals/MainView.java
@@ -5,24 +5,30 @@ import java.awt.*;
 
 @SuppressWarnings("serial")
 public class MainView extends JFrame {
-	private Container contentPane;
+	private JTabbedPane contentPane;
 
 	public MainView() {
 		setTitle("OnTheDocket");
 		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		contentPane = getContentPane();
-		contentPane.setLayout(new BorderLayout());
+		contentPane = new JTabbedPane();
+		setContentPane(contentPane);
 		
 		initializeCalendar();
+		initializeAnalytics();
 		
 		setVisible(true);
 	}
 	
 	private void initializeCalendar() {
 		Calendar calendar = new Calendar(48, 8);
-		contentPane.add(calendar, BorderLayout.CENTER);
+		contentPane.addTab("Calendar", calendar);
+	}
+	
+	private void initializeAnalytics() {
+		JPanel placeholder = new JPanel();
+		contentPane.addTab("Analytics", placeholder);
 	}
 }
 
